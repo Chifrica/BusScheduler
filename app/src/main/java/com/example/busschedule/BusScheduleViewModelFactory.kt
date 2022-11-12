cd87d0a4538ed87bb65243e332289d119609f9f7
AndroQues <chikaonwunali2019@gmail.com>
2022-11-12 09:04:15 +0100

    Initial commit

---
+++ app/src/main/java/com/example/busschedule/BusScheduleViewModelFactory.kt
@@ -0,0 +1,18 @@
+package com.example.busschedule
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.busschedule.database.schedule.ScheduleDao
+import com.example.busschedule.viewmodels.BusScheduleViewModel
+
+class BusScheduleViewModelFactory(
+    private val scheduleDao: ScheduleDao
+) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(BusScheduleViewModel::class.java)){
+            @Suppress("UNCHECKED_CAST")
+            return BusScheduleViewModel(scheduleDao) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
